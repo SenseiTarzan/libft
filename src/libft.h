e913f84fee34888d55b349203b814975cc0ce141
Gabriel Captari <gcaptari@bess-f1r1s1.clusters.42paris.fr>
2023-11-08 15:54:28 +0100

    initialise commit

--- src/libft.h
+++ src/libft.h
@@ -6,7 +6,7 @@
 /*   By: gcaptari <gcaptari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 12:48:16 by gcaptari          #+#    #+#             */
-/*   Updated: 2023/11/07 13:41:56 by gcaptari         ###   ########.fr       */
+/*   Updated: 2023/11/08 15:54:19 by gcaptari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char	*ft_strrchr(const char *str, int c);
 int		ft_memcmp(const void *v1, const void *v2, size_t n);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
-char	*ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memset(void *dest, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
