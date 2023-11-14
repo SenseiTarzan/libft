73ed3eb6dffda9d056816cebf427e1f1eeea0eb0
Gabriel Captari <gcaptari@bess-f1r2s3.clusters.42paris.fr>
2023-11-14 08:55:25 +0100

    fix trim

--- src/ft_strtrim.c
+++ src/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: gcaptari <gcaptari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 13:37:51 by gcaptari          #+#    #+#             */
-/*   Updated: 2023/11/13 15:04:06 by gcaptari         ###   ########.fr       */
+/*   Updated: 2023/11/14 08:54:43 by gcaptari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,23 +27,24 @@ static size_t	ft_strtrim_length(const char *str, size_t len,
 	const char *set, int c)
 {
 	char		*tmp;
+	size_t		i;
 
 	tmp = (char *)str;
+	i = 0;
 	if (c == 0)
-	{
-		while (*tmp && is_set_char(set, *tmp))
-			tmp++;
-	}
+		while (*tmp && is_set_char(set, *tmp++))
+			i++;
 	else if (c == 1)
 	{
-		tmp += len -1;
-		while (*tmp && is_set_char(set, *tmp))
-			tmp--;
+		tmp += len - 1;
+		while (*tmp && is_set_char(set, *tmp--))
+			i++;
 	}
-	return (tmp - str);
+	return (i);
 }
 
-char	*ft_strtrim(char *str, const char *set)
+
+char	*ft_strtrim(const char *str, const char *set)
 {
 	char							*tmp;
 	char							*new_str;
@@ -51,15 +52,17 @@ char	*ft_strtrim(char *str, const char *set)
 	const size_t					len = ft_strlen(str);
 	t_bypass_trim					trim;
 
-	trim = (t_bypass_trim){.len_start = ft_strtrim_length(str, len, set, 0),
-		.len_end = ft_strtrim_length(str, len, set, 1)};
+	trim.len_start = ft_strtrim_length(str, len, set, 0);
+	if (trim.len_start == len)
+		return (ft_calloc(1, sizeof(char)));
 	tmp = (char *)str;
+	trim.len_end = ft_strtrim_length(str, len, set, 1);
 	new_str = calloc(len - (trim.len_end + trim.len_start) + 1, sizeof(char));
 	if (!new_str)
 		return (NULL);
-	tmp = str + trim.len_start;
+	tmp = (char *)str + trim.len_start;
 	s = new_str;
-	while (*tmp && (tmp - str) <= (len - trim.len_end - 1))
+	while (*tmp && (size_t)(tmp - str) <= (len - trim.len_end - 1))
 		*s++ = *tmp++;
 	return (new_str);
 }
